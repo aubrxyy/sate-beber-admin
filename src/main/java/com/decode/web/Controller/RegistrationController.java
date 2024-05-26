98d54c8fbcafe5906e3cb023b12dd1717d252cdb
aubrey <mantiriaubrey@apps.ipb.ac.id>
2024-05-26 21:26:47 +0700

    spring boot-rkpl

--- src/main/java/com/decode/web/Controller/RegistrationController.java
+++ src/main/java/com/decode/web/Controller/RegistrationController.java
@@ -1,28 +1,28 @@
-package com.decode.web.Controller;
-
-import com.decode.web.models.UserEntity;
-import com.decode.web.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class RegistrationController {
-
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    @Autowired
-    public RegistrationController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @PostMapping("/register")
-    public UserEntity createUser(@RequestBody UserEntity user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
-    }
-}
+//package com.decode.web.Controller;
+//
+//import com.decode.web.models.UserEntity;
+//import com.decode.web.repository.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class RegistrationController {
+//
+//    private final UserRepository userRepository;
+//    private final PasswordEncoder passwordEncoder;
+//
+//    @Autowired
+//    public RegistrationController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+//        this.userRepository = userRepository;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+//
+//    @PostMapping("/register")
+//    public UserEntity createUser(@RequestBody UserEntity user) {
+//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+//        return userRepository.save(user);
+//    }
+//}
